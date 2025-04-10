be51fcee92ef2ee97b807377bf8ddf9170394b2d
JoseGarcia2006 <gonzalesgarcianacho@gmail.com>
2025-04-10 16:46:45 -0300

    Lo nuevo del jueves

--- page/js/libro.js
+++ page/js/libro.js
@@ -1,27 +1,33 @@
-async function obtenerLibro(){
-try{
-const repuesta = await fetch("./backend/routes/api.php");
-const libros = await repuesta.json();
+async function obtenerLibro() {
+    try {
+        const repuesta = await fetch("../Backend/routes/api.php?seccion=libros");
+        const libros = await repuesta.json();
 
-const contenedorLibros = document.getElementById("contenedor-libros");
-contenedorLibros.innerHTML = mostrarLibros(libros);
-}catch (error) {
-    
-console.error("Error al obtener libros: ", error);
-document.getElementById("contenedor-libros").innerHTML = "<p>Error al obtener libros</p>";
+        const contenedorLibros = document.getElementById("contenedor-libros");
+        contenedorLibros.innerHTML = mostrarLibros(libros);
+    } catch (error) {
+
+        console.error("Error al obtener libros: ", error);
+        document.getElementById("contenedor-libros").innerHTML = "<p>Error al obtener libros</p>";
 
     }
 
 }
 
-function mostrarLibros(libros){
+function mostrarLibros(libros) {
+    console.log(libros);
     let texto = "";
     libros.forEach(libro => {
-        texto += "<h4>$(libro.titulo)</h4>"
-        texto += "<p>Autor: $(libro.autor)</p>"
-        texto += "<p>Genero: $(libro.genero)</p>"
-        texto += "<hr>; // Separador entre libros"
-        
+        texto += `<tr>`
+        texto += `<td>${libro.id}</td>`
+
+        texto += `<td>${libro.titulo}</td>`
+        texto += `<td>${libro.autor}</td>`
+        texto += `<td>${libro.fecha}</td>`
+       
+        texto += `<td>${libro.disponible}</td>`
+        texto += `</tr>`
+
     });
     return texto;
 }
